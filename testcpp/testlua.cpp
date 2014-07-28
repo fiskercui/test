e1f736e6283ff500f107228b5f7e9d2a496c539f
崔卫华 <fisker.cui@gmail.com>
2014-07-29 01:56:28 +0800

    add lua support

--- testcpp/testlua.cpp
+++ testcpp/testlua.cpp
@@ -231,11 +231,88 @@ void testCppFunction()
 }
 
 
+
+
+
+int count(lua_State* L)
+{
+   //得到UpValue
+  double m_ = lua_tonumber(L, lua_upvalueindex(1));
+  //更改UpValue
+  lua_pushnumber(L, ++m_);
+  lua_replace(L, lua_upvalueindex(1));
+  //返回结果（直接复制一份UpValue作为结果）
+  lua_pushvalue(L, lua_upvalueindex(1));
+  return 1;
+}
+
+int newCount(lua_State* L)
+{
+ //计数器初值（即UpValue）
+  lua_pushnumber(L,0);
+ //放入计算函数，告诉它与这个函数相关联的数据个数
+  lua_pushcclosure(L, count, 1);
+  return 1; //一个结果,即函数体
+}
+
+void testClosure()
+{
+    char *szLua_code =
+           "c1 = NewCount() "
+           "c2 = NewCount() "
+           "for i=1,5 do print(c1()) end "
+           "for i=1,5 do print(c2()) end";
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    lua_register(L,"NewCount",newCount);
+    bool err = luaL_loadstring(L, szLua_code) || lua_pcall(L, 0, 0, 0);
+    if (err)
+    {
+      fprintf(stderr, "%s", lua_tostring(L, -1));
+      lua_pop(L, 1);   //pop error message from the stack  
+    }
+    lua_close(L);
+}
+
+
+
+//test index 
+void testGlobalIndex()
+{
+    char *szLua_code =
+           "a = 10 "
+           "b=\"hello\" "
+           "c=true ";
+
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L); 
+    bool err = luaL_loadstring(L, szLua_code) || lua_pcall(L, 0, 0, 0);
+    if (err)
+    {
+      fprintf(stderr, "%s", lua_tostring(L, -1));
+      lua_pop(L, 1);   //pop error message from the stack  
+    }
+    else
+    {
+      lua_pushnil(L);
+      while(0 !=  lua_next(L, LUA_GLOBALSINDEX))
+      {
+        lua_pushvalue(L, -2);
+        printf("**%s-%s\n", lua_tostring(L, -1), lua_typename(L, lua_type(L,-2)));
+        lua_pop(L, 2);
+      }
+    }
+    lua_close(L);
+
+}
+
 int main (void) 
 {
   // testTable();
   // testExchangeData();
   // testLua();
   testCppFunction();
+  testClosure();
+  testGlobalIndex();
   return 0;
 }
