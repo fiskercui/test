24642aee3bf5b6da61055c90a6f124274b5b80a0
fiskercui <weihua@weihuacuideMac-mini.local>
2014-08-01 18:13:56 +0800

    test

---
+++ testcpp/tolua++/test/tarray.h
@@ -0,0 +1,27 @@
+
+
+typedef struct
+{
+	float x;
+	float y;
+}Point;
+
+extern int a[10];
+extern Point p[10];
+extern Point* pp[10];
+
+
+extern int ma[10];
+extern Point mp[10];
+extern Point* mpp[10];
+
+typedef struct Array Array;
+struct Array
+{
+ int a[10];
+ Point p[10];
+ Point* pp[10];
+};
+
+extern Array array;
+extern Array* parray;
