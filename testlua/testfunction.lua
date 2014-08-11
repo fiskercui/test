c2744fca81e3c060dcdafae85293d9ba6dcf8795
fiskercui <weihua@weihuacuideMac-mini.local>
2014-08-11 17:01:44 +0800

    add variable param test to function

--- testlua/testfunction.lua
+++ testlua/testfunction.lua
@@ -1,5 +1,21 @@
 local t = {}
 
+local function  testarg( ... )
+	-- print("testarg", arg.n)
+
+
+	print("参数个数 : ", select("#",...))
+
+	local str = ""
+	for k, v in ipairs{...} do
+	str = str .. tostring(v) .. " "
+	end
+	print("参数所有值 : ",str)
+
+
+	print(unpack{...})
+end
+
 
 local function testcallfunc(listener)
 	print(type(listener))
@@ -12,6 +28,9 @@ function t:test (...)
 		function (code, ret , msg)
 			print("callback",code, ret, msg)
 		end)
+
+
+	testarg(3,5)
 end
 
 return t
