9ef9940b8769ce40a357940a6f7de3e038dcd8b2
fiskercui <weihua@weihuacuideMac-mini.local>
2014-08-15 18:30:06 +0800

    test

--- testgles/testopengles/src/com/test/opengl/OpenGLRenderer.java
+++ testgles/testopengles/src/com/test/opengl/OpenGLRenderer.java
@@ -5,6 +5,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLU;
 import android.opengl.GLSurfaceView.Renderer;
+import android.util.Log;
 
 public class OpenGLRenderer implements Renderer {
 	/*
@@ -14,7 +15,13 @@ public class OpenGLRenderer implements Renderer {
 	 * android.opengl.GLSurfaceView.Renderer#onSurfaceCreated(javax.
          * microedition.khronos.opengles.GL10, javax.microedition.khronos.
          * egl.EGLConfig)
+         * 
 	 */
+	
+	public Square square = new Square();
+	public float angle = 30.0f;
+	
+	
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		// Set the background color to black ( rgba ).
 		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);  // OpenGL docs.
@@ -42,6 +49,46 @@ public class OpenGLRenderer implements Renderer {
 		// Clears the screen and depth buffer.
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | // OpenGL docs.
                            GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glLoadIdentity();
+		gl.glTranslatef(0,0,-10);
+		
+		gl.glPushMatrix();
+		gl.glRotatef(angle, 0 , 0, 1);
+		square.draw(gl);
+		gl.glPopMatrix();
+	
+		// SQUARE B
+		// Save the current matrix
+		gl.glPushMatrix();
+		// Rotate square B before moving it,
+		// making it rotate around A.
+		gl.glRotatef(-angle, 0, 0, 1);
+		// Move square B.
+		gl.glTranslatef(2, 0, 0);
+		// Scale it to 50% of square A
+		gl.glScalef(.5f, .5f, .5f);
+		// Draw square B.
+		square.draw(gl);
+
+		// SQUARE C
+		// Save the current matrix
+		gl.glPushMatrix();
+		// Make the rotation around B
+		gl.glRotatef(-angle, 0, 0, 1);
+		gl.glTranslatef(2, 0, 0);
+		// Scale it to 50% of square B
+		gl.glScalef(.5f, .5f, .5f);
+		// Rotate around it's own center.
+		gl.glRotatef(angle * 10, 0, 0, 1);
+		// Draw square C.
+		square.draw(gl);
+		
+		gl.glPopMatrix();
+		gl.glPopMatrix();
+		
+		angle++;
+		Log.i("angle", "android" + angle);
+
 	}
 
 	/*
@@ -54,6 +101,7 @@ public class OpenGLRenderer implements Renderer {
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		// Sets the current view port to the new size.
 		gl.glViewport(0, 0, width, height);// OpenGL docs.
+		gl.glOrthox(0, 0, width, height, -2, 100);
 		// Select the projection matrix
 		gl.glMatrixMode(GL10.GL_PROJECTION);// OpenGL docs.
 		// Reset the projection matrix
