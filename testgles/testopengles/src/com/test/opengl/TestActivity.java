f088940c89de1038d9c790d27e483f65a70696ae
fiskercui <weihua@weihuacuideMac-mini.local>
2014-08-18 22:44:58 +0800

    commit add lighting

--- testgles/testopengles/src/com/test/opengl/TestActivity.java
+++ testgles/testopengles/src/com/test/opengl/TestActivity.java
@@ -1,8 +1,10 @@
 package com.test.opengl;
 
+import Light.LightRenderer;
 import Mesh.Cube;
 import Mesh.MeshRenderer;
 import Mesh.SimplePlane;
+import Mesh.Sphere;
 import android.app.Activity;
 import android.graphics.BitmapFactory;
 import android.opengl.GLSurfaceView;
@@ -24,22 +26,28 @@ public class TestActivity extends Activity {
  		//   		view.setRenderer(new SquareRenderer());
 
 
- 		SimplePlane plane = new SimplePlane(1, 1);
-		plane.z = 1.7f;
-		plane.rx = -65;
-		// Load the texture.
-		plane.loadBitmap(BitmapFactory.decodeResource(getResources(),
-				R.drawable.jay));
-		
-		Cube cube = new Cube(1, 1, 1);
-		cube.rx = 45;
-		cube.ry = 45;
-		
- 		MeshRenderer renderer = new MeshRenderer();
- 		//add先后顺序有影响
-// 		renderer.AddMesh(cube);
- 		renderer.AddMesh(plane);
- 		view.setRenderer(renderer); 
+// 		SimplePlane plane = new SimplePlane(1, 1);
+//		plane.z = 1.7f;
+//		plane.rx = -65;
+//		// Load the texture.
+//		plane.loadBitmap(BitmapFactory.decodeResource(getResources(),
+//				R.drawable.jay));
+//		
+//		Cube cube = new Cube(1, 1, 1);
+//		cube.rx = 45;
+//		cube.ry = 45;
+//		
+//		Sphere sphere = new Sphere();
+//		
+// 		MeshRenderer renderer = new MeshRenderer();
+// 		//add先后顺序有影响
+//// 		renderer.AddMesh(cube);
+//// 		renderer.AddMesh(plane);
+// 		renderer.AddMesh(sphere);
+// 		view.setRenderer(renderer); 
+ 		
+ 		LightRenderer lightrender = new LightRenderer();
+ 		view.setRenderer(lightrender);
 
 
    		setContentView(view);
